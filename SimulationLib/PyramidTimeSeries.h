5786445e58b0e406b660b7995022bfc6d222c309
Daryn Ramsden <daryn.ramsden@yale.edu>
2017-05-02 15:13:08 -0400

    Pyramid Time series in progress.

--- SimulationLib/PyramidTimeSeries.h
+++ SimulationLib/PyramidTimeSeries.h
@@ -9,17 +9,32 @@ namespace SimulationLib
 	// to store a collection of pyramid data collected during a simulation run
 	class PyramidTimeSeries
 	{
+	private:
+		string name; 
+		int time0;
+		vector<PyramidData> pyramids;
+
 	public:
-		PyramidTimeSeries(string name, int time0, int observationPeriodLength, int numOfCategories, vector<double> ageBreaks);
-	private: 
-		vector<PyramidData> pyramids; 
+		PyramidTimeSeries(string name, int time0, int observationPeriodLength, int numObs, int numOfCategories, vector<double> ageBreaks) : name{ name } {
+			int timeIndex = time0;
+			for (int j = 0; j < numObs; ++j) {
+				pyramids.push_back(PyramidData(timeIndex, numOfCategories, ageBreaks));
+				timeIndex += observationPeriodLength;
+			}
+		}
+
 	};
 
 	// to store a collection of prevalence data collected in the pyramid format during a simulation run
 	class PrevalencePyramid : public PyramidTimeSeries
 	{
+	private:
+		string name;
 	public:
-		PrevalencePyramid(string name, int time0, int observationPeriodLength, int numOfCategories, vector<double> ageBreaks);
+		PrevalencePyramid(string name, int time0, int observationPeriodLength, int numObs, int numOfCategories, vector<double> ageBreaks) : name{ name } {
+
+
+		}
 
 		void Update(int time, int category, double age, int increment);
 		void Update(int time, int oldCategory, int newCategory, double oldAge, double newAge, int numberMoved);
