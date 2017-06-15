174a8db50b5a002b00d7ec9bac73c10f977ed0ff
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-06-15 17:04:09 -0400

    Fixed up a few things.

---
+++ SimulationLib/Parameter.cpp
@@ -0,0 +1,15 @@
+#include <Parameter.h>
+
+namespace SimulationLib {
+  using namespace std;
+  using namespace StatisticalDistributions;
+  long double Parameter::operator()(mt19937_64 &g) {
+    if(holds_alternative<double>(dist))
+      return(get<double>(dist));
+    if(holds_alternative<shared_ptr<StatisticalDistribution<long> > >(dist))
+      return(*(get<shared_ptr<StatisticalDistribution<long> > >(dist))(g));
+    if(holds_alternative<shared_ptr<StatisticalDistribution<long double> > >
+       (dist))
+      return(*(get<shared_ptr<StatisticalDistribution<long double> > >(dist))
+	     (g));
+  }
--- SimulationLib/Parameter.h
+++ SimulationLib/Parameter.h
@@ -1,19 +1,26 @@
 #pragma once
 
-#include <string>
-//#include "StatisticalDistribution.h"
+#include <memory>
+#include "StatisticalDistribution.h"
+#include <variant>
+#include <random>
 
-using namespace std;
-
-namespace SimulationLib
-{
-
-	/*class Parameter : public StatisticalDistributions::StatisticalDistribution
-	{
-	public: 
-		Parameter(string name);
-		string name;
-		double currentSample; 
-	private:
-	};*/
+namespace SimulationLib {
+  using namespace StatisticalDistributions;
+  using namespace std;
+  class Parameter {
+  public: 
+    const variant<shared_ptr<const StatisticalDistribution<long> >,
+		  shared_ptr<const StatisticalDistribution<long double> >,
+		  double> dist;
+    const double min, max;
+    long double operator()(mt19937_64 &);
+    inline Parameter(double d) : dist(d), min(d), max(d) {}
+    inline Parameter(shared_ptr<StatisticalDistribution<long> > dist,
+		     double min, double max)
+      : dist(dist), min(min), max(max) {}
+    inline Parameter(shared_ptr<StatisticalDistribution<long double> > dist,
+		     double min, double max)
+      : dist(dist), min(min), max(max) {}
+  };
 }
