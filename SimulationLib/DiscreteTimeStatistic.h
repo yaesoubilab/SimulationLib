42ce4aa756c5c5a5b7d517e205b199be24f82779
Marcus Russi <marcusrussi@gmail.com>
2017-06-19 11:29:34 -0400

    Added comments to DiscreteTimeStatistic.h

--- SimulationLib/DiscreteTimeStatistic.h
+++ SimulationLib/DiscreteTimeStatistic.h
@@ -11,17 +11,32 @@ namespace SimulationLib
 	class DiscreteTimeStatistic
 	{
 	public:
+		// Name of the statistic bundle
 		string name;
+
+		// DiscreteTimeStatistic calculates time-independent point statistics.
+		// To record observations for later access via the ::GetObservations()
+		//   method, specify 'numOfObservationsToStore' on instantiation.
 		DiscreteTimeStatistic(string name);
 		DiscreteTimeStatistic(string name, int numOfObservationsToStore);
+
+		// Receives a value 'value', updating statistics in constant time.
+		// Records the value if 'numOfObservationsToStore' > 0. Throws error
+		//   if no room left to hold observations.
+		//
 		void Record(double value);
 
+		// Accessor methods for statistics, current to the last call of ::Record.
 		double GetSum();
 		double GetCount();
 		double GetMean();
 		double GetVariance();
 		double GetMin();
 		double GetMax();
+
+		// Returns vector of observations. Warning: will return an uninitialied
+		//   variable if DiscreteTimeStatistics was instantiated without
+		//   specification of 'numOfObservationsToStore'.
 		vector<double> GetObservations();
 
 	private:
