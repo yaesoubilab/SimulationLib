1fb537f410c3d2cdb56d12b25e3310b3c2b26150
Marcus Russi <marcusrussi@gmail.com>
2017-06-19 14:10:45 -0400

    Implemented rough version of TimeStatistics on IncidenceTimeSeries

--- SimulationLib/IncidenceTimeSeries.h
+++ SimulationLib/IncidenceTimeSeries.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include "TimeStatistic.h"
 
 using namespace std;
 
@@ -14,6 +15,8 @@ using namespace std;
 // - int
 //
 // They are already available to link against in the containing library.
+
+#define RECORD_ON_ALL -1
 
 namespace SimulationLib
 {
@@ -29,7 +32,20 @@ namespace SimulationLib
 	    // 'time0': first time point in simulation (usually 0)
 	    // 'timeMax': max time point in simulation
 	    // 'periodLength': how often recordings are aggregated
+	    //
+	    // Optional parameters:
+	    // [ 'recordPeriodLength': the number of periods to aggregate before
+	    //     recording a statistic. Must be a positive integer (1 or greater),
+	    //     or, if 'RECORD_ON_ALL' is passed, the ::Record method will be
+	    //     called on every value input.
+	    //   'stats': an instance of a TimeStatistic class (for now,
+	    //     DiscreteTimeStatistic or ContinuousTimeStatistic). Its ::Record
+	    //     method will be called in accordance with 'recordPeriodLength'. ]
 		IncidenceTimeSeries(string name, double time0, double timeMax, double periodLength);
+
+		IncidenceTimeSeries(string name, double time0, double timeMax, \
+							double periodLength, int recordPeriod,
+							TimeStatistic *stats);
 
 	    // Records a new value at time 'time' and adds it to the current
 	    //   aggregation.
@@ -37,6 +53,10 @@ namespace SimulationLib
 	    //   the function will issue an error if called with a 'time' of lower value
 	    //   than one which was seen in any previous invocation of the function.
 	    void Record(double time, T value);
+
+	    // Call at the end of the time series to trigger Record of statistics
+	    // for current period
+	    void Close(void);
 
 	    // Returns a vector containing all complete aggregations, and the current
 	    //   incomplete aggregation.
@@ -56,9 +76,13 @@ namespace SimulationLib
 		double timeMax;
 		double periodLength;
 
-		double mostRecentTime;
-		T aggregatedObservation;
+		double lastTime;
+		int    lastPeriod;
 
+		T aggregatedObservation;
 		vector<T> observations;
+
+		int recordPeriod;
+		TimeStatistic *stats;
 	};
 }
