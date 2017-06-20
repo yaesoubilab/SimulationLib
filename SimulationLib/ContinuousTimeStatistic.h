29022381a9b5c91d2f795af16b95d392c92849e6
Marcus Russi <marcusrussi@gmail.com>
2017-06-20 13:38:01 -0400

    Wrote draft implementation of ContinuousTimeStatistic; untested

--- SimulationLib/ContinuousTimeStatistic.h
+++ SimulationLib/ContinuousTimeStatistic.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -14,12 +15,18 @@ namespace SimulationLib
 		void Record(double time, double increment);
 
 		double GetMean();
+		double GetCount();
 		double GetVariance();
 		double GetMin();
 		double GetMax();
 
 	private:
+		long count;
 		double currentValue, mean, variance, min, max;
+
+		double baseTime;
+		double lastValue, lastTime, value;
+		double tot;
 	};
 
 }
