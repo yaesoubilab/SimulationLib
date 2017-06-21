e87e1af78db2cedeb333280d77568d572e21d7d0
Marcus Russi <marcusrussi@gmail.com>
2017-06-21 11:15:22 -0400

    Added comments

--- SimulationLib/TimeStatistic.h
+++ SimulationLib/TimeStatistic.h
@@ -7,6 +7,7 @@ namespace SimulationLib
     class TimeStatistic
     {
     public:
+        // All statistics libraries must implement these two methods
         virtual void Record(double value) = 0;
         virtual void Record(double time, double value) = 0;
     };
