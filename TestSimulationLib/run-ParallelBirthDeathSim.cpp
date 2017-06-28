aad1cb20cae0a6fa9ec180d4488a6bbef9adb4ac
Marcus Russi <marcusrussi@gmail.com>
2017-06-28 18:23:23 -0400

    Restructured TestSimulationLib and associated files to faciliate the folders' hosting of multiple separate simulations

---
+++ TestSimulationLib/run-ParallelBirthDeathSim.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include "ParallelBirthDeathSim.h"
+
+using namespace SimulationLib;
+
+// Parameters:
+// 1: fileName:
+//      Prefix of .csv file name (do not specify extension). Three files will
+//        be created: [fileName]-births.csv, [fileName]-deaths.csv,
+//        [filename]-population.csv
+// 2: nTrajectories:
+//      number of trajectories to run under the following parameters:
+// 3: timeMax:
+//      largest value of t to run simulation to
+// 4: periodLength:
+//      length of period, in units of t (integer)
+// 5: nPeople:
+//      initial size of population
+// 6: pDeath:
+//      individual probability of death over 1 time unit
+// 7: pBirth:
+//      individual probability of single reproduction over 1 time unit
+int main(int argc, char const *argv[])
+{
+    int i;
+    string fileName;
+    int nTrajectories, timeMax, periodLength;
+    long nPeople;
+    double pDeath, pBirth;
+
+    ParallelBirthDeathSim *pbdsim;
+
+    if (argc < 8) {
+        printf("Error: too few arguments\n");
+        exit(1);
+    }
+
+    i = 0;
+    fileName      = string(argv[++i]);
+    nTrajectories = atoi(argv[++i]);
+    timeMax       = atoi(argv[++i]);
+    periodLength  = atoi(argv[++i]);
+    nPeople       = atol(argv[++i]);
+    pDeath        = stof(argv[++i], NULL);
+    pBirth        = stof(argv[++i], NULL);
+
+    printf("Args:\n\tfileName=%s\n\tnTrajectories=%d\n\ttimeMax=%d\n\tperiodLength=%d\n\tnPeople=%ld\n\tpDeath=%4.4f\n\tpBirth=%4.4f\n\n", fileName.c_str(), nTrajectories, timeMax, periodLength, nPeople, pDeath, pBirth);
+
+    pbdsim = new ParallelBirthDeathSim(fileName, nTrajectories, timeMax, periodLength, nPeople, pDeath, pBirth);
+    pbdsim->Run();
+    delete pbdsim;
+
+    return 0;
+}
