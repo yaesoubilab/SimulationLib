64211fdf5de38bfe920e2de7c0e55679f324fc09
Marcus Russi <marcusrussi@gmail.com>
2017-07-06 14:14:08 -0400

    Removed old CSVExport implementation and interface

--- SimulationLib/CSVExport.cpp
+++
@@ -1,9 +0,0 @@
-#include "CSVExport-inl.h"
-
-namespace SimulationLib
-{
-    template class CSVExport<double>;
-    template class CSVExport<int>;
-    template class TimeSeriesCSVExport<int>;
-    // Add more supported types here...
-}
