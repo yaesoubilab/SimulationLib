92daae44e250f3dddcb515489f16f16955762dbf
Marcus Russi <marcusrussi@gmail.com>
2017-08-03 16:08:29 -0400

    Brought Likelihood library to compilation, untested

--- SimulationLib/src/Likelihood-inl.h
+++ SimulationLib/src/Likelihood-inl.h
@@ -3,19 +3,35 @@
 using namespace std;
 using namespace SimulationLib;
 
-template <class Distribution, typename TimeT, typename ValueT>
-auto LikelihoodFunction::operator()(TimeT t, ValueT v) {
+template <class D, typename TT, typename VT>
+typename LikelihoodFunction<D,TT,VT>::LikelihoodFunctionT
+LikelihoodFunction<D,TT,VT>::GetLikelihoodFunction(void) {
+
+    // Explicitly capture 'this'. Note that if 'this' becomes invalid due
+    //   due object destruction, this lambda will have undefined and unsafe
+    //   behavior!
+    return [this] (TT t, VT v) {
+        return Likelihood(t, v);
+    };
+}
+
+template <class D, typename TT, typename VT>
+typename LikelihoodFunction<D,TT,VT>::ProbabilityT
+LikelihoodFunction<D,TT,VT>::Likelihood(TT t, VT v) {
 
     // 'y' is the value of the target function at time 't'
-    ValueT y = f(t);
+    VT y = f(t);
 
-    // Ask for parameters of the distribution for the given 't' and 'y'
-    BoundInitializerArgs params = dpg(t, y);
-
-    // (Re-)Initialize distribution using these parameters
-    std::apply(dist.init, params);
+    // Ask for a distribution on the value of f(t) the given 't' and 'y'
+    dist = dg(t, y);
 
     // Return the probability of function 'f' being valued 'v' at time 't'
     //   by calling the new probability density function with value 'v'
     return dist.pdf(v);
 }
+
+// Implements the () operator for the class by forwarding to the public
+//   member function 'Likelihood'
+template <class D, typename TT, typename VT>
+typename LikelihoodFunction<D,TT,VT>::ProbabilityT
+LikelihoodFunction<D,TT,VT>::operator()(TT t, VT v) { return Likelihood(t, v); }
--- SimulationLib/src/Likelihood.cpp
+++ SimulationLib/src/Likelihood.cpp
@@ -1,10 +1,11 @@
 #include "Likelihood-inl.h"
+#include <Bernoulli.h>
 
 namespace SimulationLib
 {
-    using namespace StatisticalDistributionsLib;
+    using namespace StatisticalDistributions;
 
-    template class LikelihoodFunction<Bernoulli, int(int)>;
-    template class LikelihoodFunction<Bernoulli, int(double)>;
+    template class LikelihoodFunction<Bernoulli, int, int>;
+    template class LikelihoodFunction<Bernoulli, int, double>;
     // Add more supported types here...
 }
