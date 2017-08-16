b40f406c3d98d1f5fdc46e45af6947b837276ed0
Marcus Russi <marcusrussi@gmail.com>
2017-08-16 15:39:43 -0400

    Wrote draft implementation of CalculateLikelihood and added many comments. Untested

--- SimulationLib/include/SimulationLib/Calibrate.h
+++ SimulationLib/include/SimulationLib/Calibrate.h
@@ -3,71 +3,213 @@
 #include <algorithm>
 #include <exception>
 
+#include "Likelihood.h"
+#include "Likelihood-adaptors.h"
+#include "Likelihood-sums.h"
+
 namespace SimulationLib {
 
-// CalculateLikelihood:
-//     Container &model, Container &data, vector<QueryType>, DistributionGenerator
-//       -> product of likelihoods (using Likelihood-sums.h)
+// CalculateLikelihood takes model data (from one trajectory, or many) and
+//   calculates the probability that the model data would match the
+//   observational data ('obs'), provided that any element of model data is
+//   valued on a distribution described by the lambda function 'dg'.
 //
-// template
-// <class Container,
-//  class Distribution,
-//  typename PrT       = long double,
-//  typename QuerySig  = typename Container::query_signature,
-//  typename QueryType = typename Container::query_type,
-//  typename Params    = std::vector<QueryType>,
-//  class LFn          = class LikelihoodFunction<Distribution, QuerySig>,
-//  typename DG        = LFn::DistributionGenerator>
+// 1. With one set of model data, one set of observational data, and a set of
+//   parameters on which to query the model and observational data,
+//   CalculateLikelihood will evaluate the likelihood, for each member of
+//   'params', that model and observational data would match. The lg-sum of
+//   these likelihoods is then returned.
+//
+// 2. With one or more sets of model data, one set of observational data, and a set
+//   of parameters on which to query the model and observational data,
+//   CalculateLikelihood will query all model data on a query, and then use the
+//   lambda function 'modelReducer' to collapse the results of each query into
+//   a single value (ex. an average or median). In this way, we build a function
+//   'f' which acts as a composite of the model trajectories and interfaces
+//   with the LikelihoodFunction class to produce a Likelihood function. This
+//   function is, as in the case where there is only one set of model data,
+//   evaluated on each member of 'params', and the lg-sum of the result of these
+//   calculations is computed and returned.
+//
+// 3. (A second option for employing CalculateLikelihood with multiple sets of
+//  model data would be to calculate a lg-sum likelihood on each pair of
+//  [model data #i, observational data] for each 'i' in the set of model data,
+//  and write your own routine to produce a composite value from your computed
+//  set of lg-sum likelihoods).
+//
+// Options (1.) and (2.) correspond to the two overloads of CalculateLikelihood,
+//   respectively. To implement (3.) would require using the first overload
+//   of CalculateLikelihood within a map or similar prodedure, and implementing
+//   a routine to reduce the results of the map.
 
-// inline
-// PrT
-// CalculateLikelihood(Container &model,
-//                     Container &data,
-//                     Params     params,
-//                     DG         dg)
-// {
+template
 
-// }
+<// Container: a TimeSeries, PyramidTimeSeries, etc.
+ // QuerySig:  the signature of a query to the Container
+ // QueryType: the tuple containing the types of a query to the Container
+ // Queries:   a vector of these tuples
+ class Container,
+ typename QuerySig              = typename Container::query_signature,
+ typename QueryType             = typename Container::query_type,
+ typename Queries               = std::vector<QueryType>,
+
+ // Distribution: the type of distribution on which the model's output is valued
+ // ProbabilityT: the type of probability returned by that distribution's pdf.
+ class Distribution,
+ typename ProbabilityT          = long double,
+
+ // LFnGen: the type of Likelihood function generator corresponding to the
+ //   signature of a query on 'model' and 'obs', and corresponding to the
+ //   distribution used to value model output.
+ // DistributionGenerator: a std::function type which takes model parameters
+ //   and output and returns a distribution on that output.
+ class LFnGen                   = LikelihoodFunction<Distribution, QuerySig>,
+ typename DistributionGenerator = typename LFnGen::DistributionGenerator>
+
+inline
+ProbabilityT
+CalculateLikelihood(const Container&             model,  // model data
+                    const Container&             obs,    // observational data
+                    const Queries&               params, // params on which we query
+                    const DistributionGenerator& dg)     // function to create
+                                                         //   distributions
+{
+    // Construct Likelihood function generator on the model data
+    LFnGen lgen = LikelihoodOn(std::forward<decltype(model)>(model), dg);
+
+    // Get Likelihood function L, which calculates the probability that
+    //   some model output would occur under some set of parameters, which
+    //   correspond to types contained in QueryType
+    auto L = lgen.GetLikelihoodFunction();
+
+    // Create a lambda function 'g' which will have signature
+    //   g: QueryType... => Container::value_type
+    auto g = QueryLambdaForContainer(std::forward<decltype(obs)>(obs));
+
+    // Curry L using 'g': P will now have signature
+    //   P: QueryType => ProbabilityT
+
+    // This corresponds to the probability that under a particular parameter
+    //   set, the model data (when valued on a distribution) would match the
+    //   observational data
+    auto P = CurriedProbabilityOnG(L, g);
+
+    // Evaluate P on each set of parameters contained in 'params' and return
+    //   its lg-sum.
+    return ProbabilityLgSum(P, params);
+}
 
 
-// template<typename ValT,
-//          typename Distribution,
-//          typename PrT   = long double,
-//          typename TimeT = double>
-// inline
-// PrT
-// CalculateLikelihood(TimeSeries<ValT> &model,
-//                     typename LikelihoodFunction<Distribution, ValT(TimeT)>
-//                       ::DistributionGenerator dg,
-//                     TimeSeries<ValT> &data)
-// {
-//     using Signature = ValT(TimeT);
-//     using Params    = std::tuple<TimeT>;
-//     using ParamsVec = std::vector<Params>;
-//     using LiFn      = LikelihoodFunction<Distribution, Signature>;
+// This is the overload of CalculateLikelihood which supports computing
+//   likelihood on multiple trajectories of model data.
+template
 
-//     TimeT Time0        = std::max(model.GetTime0(),   data.GetTime0());
-//     TimeT TimeMax      = std::min(model.GetTimeMax(), data.GetTimeMax());
-//     int   PeriodLength = data.GetPeriodLength();
+<// Container:    a TimeSeries, PyramidTimeSeries, etc.
+ // Containers:   a vector of 'Container'. For instance, a vector of TimeSeries
+ // ValueT:       the type of value returned by a query to the Container. For
+ //                 instance, a TimeData<int> would return an 'int'.
+ // QuerySig:       the signature of a query to the Container
+ // QueryType: the tuple containing the types of a query to the Container
+ // Queries:   a vector of these tuples
+ // ModelReducer: a lambda that takes a vector of ValueT (a vector of values,
+ //                 each a piece of data returned from a query to a Container),
+ //                 and returns a ValueT representing some composition of these
+ //                 values (an average, median, etc...)
+ class       Container,
+ typename    Containers            = std::vector<Container &>,
+ typename    ValueT                = typename Container::value_type,
+ typename    QuerySig              = typename Container::query_signature,
+ typename    QueryType             = typename Container::query_type,
+ typename    Queries               = std::vector<QueryType>,
+ typename    ModelReducer          = function<ValueT(std::vector<ValueT>)>,
 
-//     if (model.GetPeriodLength() != data.GetPeriodLength())
-//         throw std::domain_error("Period length of model and data mismatched");
+ // Distribution: the type of distribution on which the model's output is valued
+ // ProbabilityT: the type of probability returned by that distribution's pdf.
+ class       Distribution,
+ typename    ProbabilityT          = long double,
 
-//     // ---------------------------
-//     auto gen   = LikelihoodOn(model, dg);
-//     auto L_v_t = gen.GetLikelihoodFunction();
+ // LFnGen: the type of Likelihood function generator corresponding to the
+ //   signature of a query on a model in 'models', and 'obs', and corresponding
+ //   to the distribution used to value model output.
+ // DistributionGenerator: a std::function type which takes model query
+ //   parameters and output and returns a distribution on that output.
+ class       LFnGen                = LikelihoodFunction<Distribution, QuerySig>,
+ typename    DistributionGenerator = typename LFnGen::DistributionGenerator>
 
-//     auto G     = [&data] (TimeT t) -> ValT { return data(t); };
-//     auto L_t   = CurriedProbabilityOnG(L_v_t, G);
+inline
+ProbabilityT
+CalculateLikelihood(const Containers&            models,       // models
+                    const ModelReducer&          modelReducer, // model reducer
+                    const Container&             obs,         // obs. data
+                    const Queries&               params,       // queries to test
+                                                               //   likelihood on
+                    const DistributionGenerator& dg)           // function to create
+                                                               //   distributions
+{
+    // Holds lambdas which allow queries on each model data in 'models'
+    std::vector<std::function<QuerySig>> modelLambdas {};
 
-//     // ---------------------------
-//     ParamsVec Ts{};
-//     for (double t = Time0; t < TimeMax; t += PeriodLength)
-//         Ts.emplace_back(t);
+    // Generate lambdas for each model data in 'models' and add them to the
+    //   'modelLambdas' vector
+    std::transform(models.begin(),
+                   models.end(),
+                   modelLambdas.begin(),
+                   QueryLambdaForContainer);
 
-//     return ProbabilityLgSum<PrT, TimeT>(L_t, Ts);
-// }
+    // The function 'f' to be used by the Likelihood function. 'f' is a composite
+    //   of lambas for each model data in 'models', and the composition of these
+    //   lambdas into 'f' is dictated by the 'modelReducer'.
+    std::function<QuerySig> f = [&modelReducer] (auto&&... ps) {
 
-// Bonus function
+        // Holds results of queries to each model data in 'models'
+        std::vector<ValueT> modelQueryResults{};
+
+        // Query each model data in 'models' and store the result in
+        //   'modelQueryResults'
+        std::transform(modelLambdas.begin(),
+                       modelLambdas.end(),
+                       modelQueryResults.begin(),
+                       [&ps...] (auto g) {
+                         // Forward query parameters to the lambda for a
+                         //   particular trajectory of model data
+                         g(std::forward<decltype(ps)>(ps)...);
+                       });
+
+        // Use 'modelReducer' to combine the results of the query to each
+        //   trajectory's model data into a single value of type 'ProbabilityT',
+        //   representing that the model (as opposed to, say, one trajectory
+        //   of the model) would produce the same values as 'data' under the
+        //   query parameter set 'params' and assuming that values returned by
+        //   queries to model data are valued on a distribution described by
+        //   'dg'
+        return modelReducer(modelQueryResults);
+    };
+
+    // Construct Likelihood function generator
+    LFnGen lgen{f, std::forward<decltype(dg)>(dg)};
+
+    // Get Likelihood function L, which calculates the probability that
+    //   some model output would occur under some set of parameters, which
+    //   correspond to types contained in QueryType
+    auto L = lgen.GetLikelihoodFunction();
+
+    // Create a lambda function 'g' which will have signature
+    //   g: QueryType... => Container::value_type
+    auto g = QueryLambdaForContainer(std::forward<decltype(obs)>(obs));
+
+    // Curry L using 'g': P will now have signature
+    //   P: QueryType => ProbabilityT
+
+    // This corresponds to the probability that under a particular parameter
+    //   set, the model data (when valued on a distribution) would match the
+    //   observational data
+    auto P = CurriedProbabilityOnG(L, g);
+
+    // Evaluate P on each set of parameters contained in 'params' and return
+    //   its lg-sum.
+    return ProbabilityLgSum(P, params);
+}
+
+
 
 }
